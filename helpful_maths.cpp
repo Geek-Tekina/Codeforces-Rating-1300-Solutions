7fbb7e899fad27651c5c305c05edc79bdac41e46
Geek-Tekina <83786078+Geek-Tekina@users.noreply.github.com>
2022-01-23 12:21:50 +0530

    Helpful Maths

---
+++ helpful_maths.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	string s;
+	char a[100];
+	cin >> s;
+	int j = 0;
+	for (int i = 0;i < s.length();i++)
+	{
+		if (s[i] != '+')
+		{
+			a[j] = s[i];
+			j++;
+		}
+	}
+	sort(a, a + j);
+	for (int i = 0;i < j;i++)
+	{
+		if (i == j - 1) cout << a[i] << endl;
+		else cout << a[i] << "+";
+	}
+	return 0;
+}
